d7772f5b862cd758baa19025f14b47fd946a2b0b
andrewjmfleet-create <andrew.jm.fleet@gmail.com>
2026-03-10 15:42:15 -0400

    Update character.c

--- character.c
+++ character.c
@@ -14,23 +14,152 @@ char sees_player(int player_y, int player_x, int minotaur_y, int minotaur_x) {
     // check if neither the x nor y coordinate is the same as the player
     // if there's a wall in between, they can't see
     // if one of them is the same, check if the path in between is clear
+    int start=0;
+    int end=0;
+    if (player_y == minotaur_y) {
+        if (player_x < minotaur_x) {
+            start = player_x + 1;
+            end = minotaur_x;
+        } else {
+            start = minotaur_x + 1;
+            end = player_x;
+        }
+
+        for (int i = start; i < end; i++) {
+            if (map[player_y * width + i] == WALL) {
+                return SEES_NOTHING;
+            }
+        }
+        if (minotaur_x > player_x) {
+            return 'a';
+        } else {
+            return 'd';
+        }
+    }
+    if (player_x == minotaur_x) {
+        if (player_y < minotaur_y) {
+            start = player_y + 1;
+            end = minotaur_y;
+        } else {
+            start = minotaur_y + 1;
+            end = player_y;
+        }
+        for (int i = start; i < end; i++) {
+            if (map[i * width + player_x] == WALL) {
+                return SEES_NOTHING;
+            }
+        }
+        if (minotaur_y > player_y) {
+            return 'w';
+        } else {
+            return 's';
+        }
+    }
     return SEES_NOTHING;
 }
-
 int move_character(int * y, int * x, char direction, char character) {
     // check for an invalid direction (not LEFT, RIGHT, UP, or DOWN)
     // calculate the new coordinates to use on success (store in local variables)
-    // check if the new coordinates point to a wallg
+    // check if the new coordinates point to a wall
     // at this point, the move is known to be valid (OK direction and not a wall)
     // remove character from the old position and replace with EMPTY
     // set character in the new position in map
     // update the x/y coordinate pointers
-    return MOVED_OKAY;
-}
 
+    if (direction!='w' && direction!='a' && direction!='s' && direction!='d') {
+        return MOVED_INVALID_DIRECTION;
+    } else {
+        int newy = *y;
+        int newx = *x;
+
+        switch (direction) {
+            case 'w':
+                newy--;
+                break;
+            case 'a':
+                newx--;
+                break;
+            case 's':
+                newy++;
+                break;
+            case 'd':
+                newx++;
+                break;
+        }
+
+        if (newy < 0 || newy >= height || newx < 0 || newx >= width) {
+            return MOVED_WALL;
+        }
+
+        if (map[newy * width + newx] == WALL) {
+            return MOVED_WALL;
+        } else {
+            map[(*y) * width + (*x)] = EMPTY;
+            map[newy * width + newx] = character;
+            *y = newy;
+            *x = newx;
+            return MOVED_OKAY;
+        }
+    }
+}
 int charge_minotaur(int *y, int *x, int player_y, int player_x, char charge_direction) {
     // call move_character twice or until a wall is hit
     // when the wall is hit, move the Minotaur into the wall in the direction it is charging
     // calculate the new coordinates
+    int newy=*y;
+    int newx=*x;
+    int movement=move_character(y, x, charge_direction, MINOTAUR);
+    if (movement==MOVED_WALL) {
+        switch (charge_direction) {
+            case 'w':
+                newy--;
+                break;
+            case 'a':
+                newx--;
+                break;
+            case 's':
+                newy++;
+                break;
+            case 'd':
+                newx++;
+                break;
+            default:
+                break;
+        }
+        int tempy=*y;
+        int tempx=*x;
+        map[(tempy*width)+tempx] = EMPTY;
+        map[newy*width+newx] = MINOTAUR;
+        *y=newy;
+        *x=newx;
+    }
+    movement=move_character(y, x, charge_direction, MINOTAUR);
+
+    if (movement==MOVED_WALL) {
+        switch (charge_direction) {
+            case 'w':
+                newy--;
+                break;
+            case 'a':
+                newx--;
+                break;
+            case 's':
+                newy++;
+                break;
+            case 'd':
+                newx++;
+                break;
+            default:
+                break;
+        }
+        int tempy=*y;
+        int tempx=*x;
+        map[(tempy*width)+tempx] = EMPTY;
+        map[newy*width+newx] = MINOTAUR;
+        *y=newy;
+        *x=newx;
+        return MOVED_OKAY;
+    }
+
     return MOVED_OKAY;
 }
